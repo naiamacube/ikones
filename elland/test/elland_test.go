b3ff7473fa74ef1fa29e057935aa6b0a596de80c
alxyok <alxyok@naiama.com>
2022-11-26 02:56:45 +0000

    fix packages versions. activate conda env mlops

--- elland/test/elland_test.go
+++ elland/test/elland_test.go
@@ -20,18 +20,18 @@ func TestPythonPackagesVersions(t *testing.T) {
 
 	docker.Build(t, "../", buildOptions)
 
-	// Make sure Python version is: 3.9.13
+	// Make sure Python version is: 3.9.12
 	pythonOpts := &docker.RunOptions{Command: []string{"python", "--version"}}
 	pythonOutput := docker.Run(t, *imgName, pythonOpts)
-	assert.Equal(t, "3.9.13", pythonOutput)
+	assert.Equal(t, "Python 3.9.12", pythonOutput)
 
-	// Make sure PyTorch version is: 1.13.1
-  torchOpts := &docker.RunOptions{Command: []string{"python", "-c", "import torch; torch.__version__"}}
+	// Make sure PyTorch version is: 1.13.0
+  torchOpts := &docker.RunOptions{Command: []string{"python", "-c", "import torch; print(torch.__version__)"}}
   torchOutput := docker.Run(t, *imgName, torchOpts)
-  assert.Equal(t, "1.13.1", torchOutput)
+  assert.Equal(t, "1.13.0", torchOutput)
 
 	// Make sure PyTorch Lightning version is: 1.7.7
-  lightningOpts := &docker.RunOptions{Command: []string{"python", "-c", "import pytorch_lightning; pytorch_lightning.__version__"}}
+  lightningOpts := &docker.RunOptions{Command: []string{"python", "-c", "import pytorch_lightning; print(pytorch_lightning.__version__)"}}
   lightningOutput := docker.Run(t, *imgName, lightningOpts)
   assert.Equal(t, "1.7.7", lightningOutput)
 }
